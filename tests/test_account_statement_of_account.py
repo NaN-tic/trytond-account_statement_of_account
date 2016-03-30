eadb6373f075e6d75f1657038763836402314232
resteve <resteve@zikzakmedia.com>
2016-03-30 10:50:01 +0200

    Reactive RST tests scenarios

--- tests/test_account_statement_of_account.py
+++ tests/test_account_statement_of_account.py
@@ -7,6 +7,7 @@ from decimal import Decimal
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
 from trytond.transaction import Transaction
 
@@ -167,5 +168,6 @@ def suite():
     suite.addTests(doctest.DocFileSuite(
             'scenario_statement_of_account.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
