5f9f29c933fa659a2a092cda686f2242d0584d20
C?dric Krier <ced@b2ck.com>
2018-01-11 11:47:36 +0100

    Remove volumes from drone and fix test

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,8 @@
 # The COPYRIGHT file at the top level of this repository contains the full 
 # copyright notices and license terms.
-
-from .test_account_statement_of_account import suite
+try:
+    from trytond.modules.account_statement_of_account.tests.test_account_statement_of_account import suite
+except ImportError:
+    from .test_account_statement_of_account import suite
 
 __all__ = ['suite']
